8beedceee1c06434a15b5667250d2e294d2a324f
aldaonggar <nur.aldaonggar@student.kuleuven.be>
2022-12-06 20:46:49 +0100

    added initial temporary drawer

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,24 @@
-import { Box, Divider } from '@mui/material';
+import { Box, Divider, Typography } from '@mui/material';
 import React from 'react';
+import { SettingsDrawer } from './components/drawer/drawer';
 import Map from './components/map/map'
 
 function App() {
   return (
     <div className="App">
+      <SettingsDrawer/>
       <Map/>
       <Divider/>
-      <Box sx={{
-        backgroundColor: 'primary.dark',
-        width: 1000
-        }} />
+      <Box
+        sx={{
+          width: 300,
+          height: 50,
+        }}
+      >
+        <Typography variant="h6" component="div">
+          ONLINE: 
+        </Typography>
+      </Box>
     </div>
   );
 }
